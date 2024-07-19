55439d81f1976b8f9629546cfed4d3f605661239
FatmaAtta <fatmamaali@gmail.com>
2024-07-19 11:11:45 +0300

    modified banner bg-image

---
+++ src/components/banner/Banner.js
@@ -0,0 +1,21 @@
+import './banner.css';
+
+export const Banner = () => {
+    return(
+    <section className="banner" id="home">
+        <span className="left-col">
+            <span className="welcome"></span>
+            <span className="animated-text"></span>
+            <p className="summary"></p>
+            <div className="connect">
+                <span className="connect-text"></span>
+                <span className="connect-icon"></span>
+            </div>
+        </span>
+        <span className="right-col">
+
+
+        </span>
+    </section>
+    )
+}

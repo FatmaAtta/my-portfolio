0914ec9ab1516e8e86d8c26de95011766f183a05
FatmaAtta <fatmamaali@gmail.com>
2024-08-29 17:41:29 +0300

    created project grid component

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,6 +1,6 @@
 export const Project = () => {
 
     return(
-        <></>
+        <>project</>
     );
 }
---
+++ src/components/projects/ProjectsGrid.js
@@ -0,0 +1,8 @@
+export const ProjectsGrid = () => {
+
+    return(
+        <>
+        Hello
+        </>
+    );
+}

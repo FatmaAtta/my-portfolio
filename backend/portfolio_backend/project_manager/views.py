f8d3f3954ee92f518dc9c551d984380db67015f6
FatmaAtta <fatmamaali@gmail.com>
2024-08-30 22:32:37 +0300

    created a serializer

--- backend/portfolio_backend/project_manager/views.py
+++ backend/portfolio_backend/project_manager/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Project
+from .serializers import ProjectSerializer
 # Create your views here.
+
+# View to list all projects
+class ProjectListView(generics.ListAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+# View to retrieve a specific project
+class ProjectDetailView(generics.RetrieveAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
